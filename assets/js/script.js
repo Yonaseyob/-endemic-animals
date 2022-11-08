69b993b04a6e01051463ada7d9f33a721765bf39
Yonaseyob <yonaseyobberhane@gmail.com>
2022-11-08 15:57:57 +0000

    Add the fist function code to the script.jsS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
+
+
+function welcome() {
+
 let userName = document.querySelector(".userName");
 let welcomeText = document.querySelector(".welcome-text");
 let welcomeArea = document.querySelector(".welcome-area");
 
-function welcome() {
     welcomeText.innerHTML = ""
     welcomeText.innerHTML = "Hello! " + userName.value;
     welcomeArea.style.display = "none";
+    
 }
+
+
