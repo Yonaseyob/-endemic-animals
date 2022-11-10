9e515910e50d26c97a407d5854e487768e534f60
Yonaseyob <yonaseyobberhane@gmail.com>
2022-11-10 13:39:44 +0000

    Working on new ideas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,51 @@
 
 
+const quizData = [
+{
+    question: "how many players are in a football team?",
+    option1: "2",
+    option2: "4",
+    option3: "11",
+    option4: "5",
+    correctAswer: "11"
+},
+{
+    question: "how are you?",
+    option1: "2",
+    option2: "4",
+    option3: "11",
+    option4: "5",
+    correctAswer: "11"
+},
+{
+    question: "what is my age?",
+    option1: "2",
+    option2: "4",
+    option3: "11",
+    option4: "5",
+    correctAswer: "11"
+},
+]
+
+const quizContainer = document.getElementById("quiz-container");
+var randomQuiz = quizData[Math.floor(Math.random()*quizData.length)];
+
+function displayQuiz (quiz) {
+
+     quizContainer.innerHTML = `
+     <div class = "quiz-area">
+        <p>${quiz.question}</p><hr>
+        <input type="radio" name=${quiz.option1}>${quiz.option1}<br>
+        <input type="radio" name=${quiz.option2}>${quiz.option2}<br>
+        <input type="radio" name=${quiz.option3}>${quiz.option3}<br>
+        <input type="radio" name=${quiz.option4}>${quiz.option4}<br>
+        <button class="next" onclick="next()">NEXT</button>
+    </div>
+     `
+}
+
+displayQuiz(randomQuiz)
+
 function welcome() {
 
 let userName = document.querySelector(".userName");
@@ -15,10 +61,11 @@ let welcomeArea = document.querySelector(".welcome-area");
 
 document.getElementsByClassName("quiz-area")[0].style.display = "block";
 
-function next(id) {
-
-    document.getElementsByClassName("quiz-area")[id-1].style.display = "none";
-    document.getElementsByClassName("quiz-area")[id].style.display = "block";
+function next() {
+    var randomQuiz = quizData[Math.floor(Math.random()*quizData.length)];
+    displayQuiz(randomQuiz)
+  //  document.getElementsByClassName("quiz-area")[id-1].style.display = "none";
+   // document.getElementsByClassName("quiz-area")[id].style.display = "block";
 
     
 }
@@ -45,12 +92,7 @@ function score() {
     if (document.getElementById("correct5").checked){
         score++;
     }
-     alert("naice:" + score);
-     correctAswer();
-    } else {
-        alert("not");
-        incorrectAaswer();
-     
+    
 
 }
 
