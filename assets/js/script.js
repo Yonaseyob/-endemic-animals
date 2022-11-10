3b4a1e27647c6a5d1d8147c65c521847f0bd95b6
Yonaseyob <yonaseyobberhane@gmail.com>
2022-11-10 07:58:30 +0000

    Add score trackingg function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,34 +6,51 @@ let userName = document.querySelector(".userName");
 let welcomeText = document.querySelector(".welcome-text");
 let welcomeArea = document.querySelector(".welcome-area");
 
-
     welcomeText.innerHTML = ""
     welcomeText.innerHTML = "Hello! " + userName.value;
     welcomeArea.style.display = "none";
     
 }
 
-function quizArea() {
-    
-    let quizArea = document.getElementsByClassName("quiz-are");
-    for (quizArea of quizArea);
 
-    quizArea.style.display[0] = "block";
-    
-    
+document.getElementsByClassName("quiz-area")[0].style.display = "block";
 
+function next(id) {
+
+    document.getElementsByClassName("quiz-area")[id-1].style.display = "none";
+    document.getElementsByClassName("quiz-area")[id].style.display = "block";
+
+    
 }
+
 
 function score() {
     let score = 0;
+    
     if (document.getElementById("correct1").checked){
         score++;
+    }
+    if (document.getElementById("correct2").checked){
+        score++;
+    }
+
+    if (document.getElementById("correct3").checked){
+        score++;
+    }
+
+    if (document.getElementById("correct4").checked){
+        score++;
+    }
+
+    if (document.getElementById("correct5").checked){
+        score++;
+    }
      alert("naice:" + score);
      correctAswer();
     } else {
         alert("not");
         incorrectAaswer();
-    }
+     
 
 }
 
