fd74174c077517e93fabbcf38b5da2339d0bfc3d
Yonaseyob <yonaseyobberhane@gmail.com>
2022-11-10 17:01:04 +0000

    Add quiz data and random question selector to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 const quizData = [
 {
-    question: "how many players are in a football team?",
+    question: "Q1 how many players are in a football team?",
     option1: "2",
     option2: "4",
     option3: "11",
@@ -25,14 +25,30 @@ const quizData = [
     option4: "5",
     correctAswer: "11"
 },
+{
+    question: "what is my age?",
+    option1: "2",
+    option2: "4",
+    option3: "11",
+    option4: "5",
+    correctAswer: "11"
+},
+{
+    question: "what is my age?",
+    option1: "2",
+    option2: "4",
+    option3: "11",
+    option4: "5",
+    correctAswer: "11"
+},
 ]
 
 const quizContainer = document.getElementById("quiz-container");
-var randomQuiz = quizData[Math.floor(Math.random()*quizData.length)];
+let randomQuiz = quizData[Math.floor(Math.random()*quizData.length)];
 
 function displayQuiz (quiz) {
 
-     quizContainer.innerHTML = `
+     quizContainer.innerHTML =`
      <div class = "quiz-area">
         <p>${quiz.question}</p><hr>
         <input type="radio" name=${quiz.option1}>${quiz.option1}<br>
@@ -41,8 +57,7 @@ function displayQuiz (quiz) {
         <input type="radio" name=${quiz.option4}>${quiz.option4}<br>
         <button class="next" onclick="next()">NEXT</button>
     </div>
-     `
-}
+     `}
 
 displayQuiz(randomQuiz)
 
@@ -64,50 +79,16 @@ document.getElementsByClassName("quiz-area")[0].style.display = "block";
 function next() {
     var randomQuiz = quizData[Math.floor(Math.random()*quizData.length)];
     displayQuiz(randomQuiz)
-  //  document.getElementsByClassName("quiz-area")[id-1].style.display = "none";
-   // document.getElementsByClassName("quiz-area")[id].style.display = "block";
-
-    
+  
 }
+ function checkAnswer(){
+    let userAnswer = quizData.correctAswer;
+    let isCorrect = userAnswer === correctAswer[0];
 
-
-function score() {
-    let score = 0;
-    
-    if (document.getElementById("correct1").checked){
-        score++;
+    if (isCorrect) {
+        alert("nn" + score);
     }
-    if (document.getElementById("correct2").checked){
-        score++;
-    }
-
-    if (document.getElementById("correct3").checked){
-        score++;
-    }
-
-    if (document.getElementById("correct4").checked){
-        score++;
-    }
-
-    if (document.getElementById("correct5").checked){
-        score++;
-    }
-    
-
-}
-
-function correctAswer() {
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
-}
-
-function incorrectAaswer() {
-    let oldScore = parseInt(document.getElementById("incorrect").innerText);
-    document.getElementById("incorrect").innerText = ++oldScore;
-    
-}
-
-
+ }
 
 
 
