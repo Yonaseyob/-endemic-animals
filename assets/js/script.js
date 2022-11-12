1802f093544c7415b9789c0b265a99845b3adcca
Yonaseyob <yonaseyobberhane@gmail.com>
2022-11-12 13:15:32 +0000

    Add loadQuiz function to load quistios on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,94 +1,136 @@
 
-
 const quizData = [
-{
-    question: "Q1 how many players are in a football team?",
-    option1: "2",
-    option2: "4",
-    option3: "11",
-    option4: "5",
-    correctAswer: "11"
-},
-{
-    question: "how are you?",
-    option1: "2",
-    option2: "4",
-    option3: "11",
-    option4: "5",
-    correctAswer: "11"
-},
-{
-    question: "what is my age?",
-    option1: "2",
-    option2: "4",
-    option3: "11",
-    option4: "5",
-    correctAswer: "11"
-},
-{
-    question: "what is my age?",
-    option1: "2",
-    option2: "4",
-    option3: "11",
-    option4: "5",
-    correctAswer: "11"
-},
-{
-    question: "what is my age?",
-    option1: "2",
-    option2: "4",
-    option3: "11",
-    option4: "5",
-    correctAswer: "11"
-},
-]
+    {
+        question: "Which one of the followin animals is endemic to China??",
+        a: "Three-Toed Sloth",
+        b: "Giant Panda",
+        c: "Poisen Dart Frog",
+        correct: "b",
+    },
+    {
+        question: "Which one of the followin animals is endemic to Costarica?",
+        a: "Sclater's Monkey",
+        b: "Three-Toed Sloth",
+        c: "Poisen Dart Frog",
+        correct: "c",
+    },
+    {
+        question: "Which one of the followin animals is endemic to USA?",
+        a: "Patagonian Mar",
+        b: "Glacier Bear",
+        c: "Wilson's Beird of Paradise",
+        correct: "b",
+    },
+    {
+        question: "Which one of the followin animals is endemic to Nigeria?",
+        a: "Pygmy Three-Toed Sloth",
+        b: "Sclater's Monkey",
+        c: "Poisen Dart Frog",
+        correct: "b",
+    },
+    {
+        question: " Which one of the followin animals is endemic to Argentina?",
+        a: "Wilson's Beird of Paradise",
+        b: "Patagonian Mara",
+        c: "Sclater's Monkey",
+        correct: "b",
+    },
+];
 
-const quizContainer = document.getElementById("quiz-container");
-let randomQuiz = quizData[Math.floor(Math.random()*quizData.length)];
-
-function displayQuiz (quiz) {
-
-     quizContainer.innerHTML =`
-     <div class = "quiz-area">
-        <p>${quiz.question}</p><hr>
-        <input type="radio" name=${quiz.option1}>${quiz.option1}<br>
-        <input type="radio" name=${quiz.option2}>${quiz.option2}<br>
-        <input type="radio" name=${quiz.option3}>${quiz.option3}<br>
-        <input type="radio" name=${quiz.option4}>${quiz.option4}<br>
-        <button class="next" onclick="next()">NEXT</button>
-    </div>
-     `}
-
-displayQuiz(randomQuiz)
+const quiz = document.getElementById('quiz');
+const answerElements = document.querySelectorAll('.answer');
+const questionElement = document.getElementById('question' );
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const submit = document.getElementById('submit');
 
 function welcome() {
 
-let userName = document.querySelector(".userName");
-let welcomeText = document.querySelector(".welcome-text");
-let welcomeArea = document.querySelector(".welcome-area");
-
-    welcomeText.innerHTML = ""
-    welcomeText.innerHTML = "Hello! " + userName.value;
-    welcomeArea.style.display = "none";
+    let userName = document.querySelector(".userName");
+    let welcomeText = document.querySelector(".welcome-text");
+    let welcomeArea = document.querySelector(".welcome-area");
     
-}
-
-
-document.getElementsByClassName("quiz-area")[0].style.display = "block";
-
-function next() {
-    var randomQuiz = quizData[Math.floor(Math.random()*quizData.length)];
-    displayQuiz(randomQuiz)
-  
-}
- function checkAnswer(){
-    let userAnswer = quizData.correctAswer;
-    let isCorrect = userAnswer === correctAswer[0];
-
-    if (isCorrect) {
-        alert("nn" + score);
+        welcomeText.innerHTML = ""
+        welcomeText.innerHTML = "Hello! " + userName.value;
+        welcomeArea.style.display = "none";
+        
     }
- }
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+const currentQuizData = quizData[currentQuiz];
+
+questionElement.innerText = currentQuizData.question;
+a_text.innerText = currentQuizData.a;
+b_text.innerText = currentQuizData.b;
+c_text.innerText = currentQuizData.c;
+}
+
+function deselectAnswers(){
+    answerElements.forEach(answerEl => answerEl.checked = false);
+    }
+
+function getSelected(){
+    let answer;
+
+    answerElements.forEach(answerEl => {
+        if(answerEl.checked){
+            answer = answerEl. id;
+         }
+       
+    });
+
+    return answer;
+}
+
+submit.addEventListener('click', () => {
+    const answer = getSelected();
+    
+    if(answer){
+      if(answer === quizData[currentQuiz] .correct){
+        score ++;
+
+        incrementScore();
+      }else {
+        incrementWrongAnswer();
+
+      }
+
+      currentQuiz ++;
+
+      if(currentQuiz < quizData.length){
+        loadQuiz();
+      }
+
+       else{
+        quiz.innerHTML = `<h2>You answered corectly at ${score}/${quizData.length} questions</h2>
+        <button onclick = "location.reload()">Reload</button>`;
+       }
+    }
+
+});
+    
+function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
+}
+
+function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+}
+
+
+
+
 
 
 
